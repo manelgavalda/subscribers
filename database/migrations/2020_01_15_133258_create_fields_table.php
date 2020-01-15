c0728ebbfe64e287c1222cfe1cc4b6fca16cae65
Manel Gavaldà Andreu <manelgavalda1@gmail.com>
2020-01-15 14:37:52 +0100

    Add Subscriber and Field resources

---
+++ database/migrations/2020_01_15_133258_create_fields_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFieldsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fields', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->enum('type', ['date', 'number', 'string', 'boolean']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fields');
+    }
+}
